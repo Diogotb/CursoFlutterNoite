8214d31dc8dd25664143052935d5fcb05a9066ff
Diogotb <digogo@gmail.com>
2024-05-02 21:56:13 -0300

    02/05

--- projeto_json/lib/View/cadastro_carro_view.dart
+++ projeto_json/lib/View/cadastro_carro_view.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CarroCadastroScreen extends StatefulWidget {
   const CarroCadastroScreen({super.key});
@@ -8,7 +11,11 @@ class CarroCadastroScreen extends StatefulWidget {
 }
 
 class _CarroCadastroScreenState extends State<CarroCadastroScreen> {
+  final _formKey = GlobalKey<FormState>();
   TextEditingController _placaController = TextEditingController();
+  TextEditingController _modeloController = TextEditingController();
+  File? _imagemSelecionada;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,26 +25,77 @@ class _CarroCadastroScreenState extends State<CarroCadastroScreen> {
       body: Padding(
         padding: EdgeInsets.all(12),
         child: Center(
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  controller: _placaController,
-                  decoration: const InputDecoration(labelText: 'Placa'),
-                  validator: (value) {
-                    if (value!.trim().isEmpty) {
-                      return 'Placa Não Pode Ser Vazia';
-                    } else {
-                      return null;
-                    }
-                  },
-                )
-              ],
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _placaController,
+                    decoration: const InputDecoration(labelText: 'Placa'),
+                    validator: (value) {
+                      if (value!.trim().isEmpty) {
+                        return 'Placa Não Pode Ser Vazia';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    controller: _modeloController,
+                    decoration: const InputDecoration(labelText: 'Modelo'),
+                    validator: (value) {
+                      if (value!.trim().isEmpty) {
+                        return 'Placa Não Pode Ser Vazia';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _imagemSelecionada != null
+                      ? Image.file(
+                          _imagemSelecionada!,
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        )
+                      : SizedBox.shrink(),
+                  ElevatedButton(
+                    onPressed: _tirarFoto,
+                    child: Text('Tirar Foto'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _cadastrarCarro();
+                      }
+                    },
+                    child: Text("Cadastrar"),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _tirarFoto() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() {
+        _imagemSelecionada = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _cadastrarCarro() {
+    //cadastrar
   }
 }
